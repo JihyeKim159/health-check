a9dd12dd1e8cc154267889815201b057116b845c
jihye <jihye.kim159@gmail.com>
2025-04-20 07:51:19 -0400

    Create the new file in case of detached HEAD

--- all_checks.py
+++ all_checks.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import shutil
+import socket
 
 def check_disk_full(disk, min_gb, min_percent):
     """Returns True if there isn't enough disk space, False otherwise."""
@@ -23,10 +24,20 @@ def check_reboot():
     #Returns True if the computer has a pending reboot.
     return os.path.exists("/run/reboot-required")
 
+def check_no_network():
+    #Returns True if it fails to resolve Google's URL, False otherwise
+    try:    
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
+    return os.path.exists("/run/reboot-required")
 def main(): 
     checks=[
             (check_reboot, "Pending Reboot"),
             (check_root_full, "Root partition full"),
+            (check_no_network, "No working Network."),
     ]
     everything_ok = True
     for check, msg in checks:
